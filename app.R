a4b9404a6faa2284aae3e1d1deccb0cc94a03d6a
Júlio Rafael <33831855+jmrafael@users.noreply.github.com>
2021-08-08 18:33:12 +0200

    New app file

--- app.R
+++ app.R
@@ -1,49 +1,129 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(shinydashboard)
+library(DT)
+library(shinyjs)
+library(sodium)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+# Main login screen
+loginpage <- div(id = "loginpage", style = "width: 500px; max-width: 100%; margin: 0 auto; padding: 20px;",
+                 wellPanel(
+                   tags$h2("ENTRADA", class = "text-center", style = "padding-top: 0;color:#333; font-weight:600;"),
+                   textInput("userName", placeholder="Username", label = tagList(icon("user"), "Username")),
+                   passwordInput("passwd", placeholder="Password", label = tagList(icon("unlock-alt"), "Password")),
+                   br(),
+                   div(
+                     style = "text-align: center;",
+                     actionButton("login", "ENTRAR", style = "color: white; background-color:#3c8dbc;
+                                 padding: 10px 15px; width: 150px; cursor: pointer;
+                                 font-size: 18px; font-weight: 600;"),
+                     shinyjs::hidden(
+                       div(id = "nomatch",
+                           tags$p("Oops! Incorrect username or password!",
+                                  style = "color: red; font-weight: 600; 
+                                            padding-top: 5px;font-size:16px;", 
+                                  class = "text-center"))),
+                     br(),
+                     br(),
+                     tags$code("Nome de Utilizador: utilizador1  Senha de acesso: senha1"),
+                     br(),
+                     tags$code("SNome de Utilizador: utilizador2  Senha de acesso: senha2")
+                   ))
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+credentials = data.frame(
+  username_id = c("utilizador1", "utilizador2"),
+  passod   = sapply(c("senha1", "senha1"),password_store),
+  permission  = c("basic", "advanced"), 
+  stringsAsFactors = F
+)
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+header <- dashboardHeader( title = "R ShinyDashboard", uiOutput("logoutbtn"))
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+sidebar <- dashboardSidebar(uiOutput("sidebarpanel")) 
+body <- dashboardBody(shinyjs::useShinyjs(), uiOutput("body"))
+ui<-dashboardPage(header, sidebar, body, skin = "blue")
+
+server <- function(input, output, session) {
+  
+  login = FALSE
+  USER <- reactiveValues(login = login)
+  
+  observe({ 
+    if (USER$login == FALSE) {
+      if (!is.null(input$login)) {
+        if (input$login > 0) {
+          Username <- isolate(input$userName)
+          Password <- isolate(input$passwd)
+          if(length(which(credentials$username_id==Username))==1) { 
+            pasmatch  <- credentials["passod"][which(credentials$username_id==Username),]
+            pasverify <- password_verify(pasmatch, Password)
+            if(pasverify) {
+              USER$login <- TRUE
+            } else {
+              shinyjs::toggle(id = "nomatch", anim = TRUE, time = 1, animType = "fade")
+              shinyjs::delay(3000, shinyjs::toggle(id = "nomatch", anim = TRUE, time = 1, animType = "fade"))
+            }
+          } else {
+            shinyjs::toggle(id = "nomatch", anim = TRUE, time = 1, animType = "fade")
+            shinyjs::delay(3000, shinyjs::toggle(id = "nomatch", anim = TRUE, time = 1, animType = "fade"))
+          }
+        } 
+      }
+    }    
+  })
+  
+  output$logoutbtn <- renderUI({
+    req(USER$login)
+    tags$li(a(icon("fa fa-sign-out"), "Logout", 
+              href="javascript:window.location.reload(true)"),
+            class = "dropdown", 
+            style = "background-color: #eee !important; border: 0;
+                    font-weight: bold; margin:5px; padding: 10px;")
+  })
+  
+  output$sidebarpanel <- renderUI({
+    if (USER$login == TRUE ){ 
+      sidebarMenu(
+        menuItem("Main Page", tabName = "dashboard", icon = icon("dashboard")),
+        menuItem("Second Page", tabName = "second", icon = icon("th"))
+      )
+    }
+  })
+  
+  output$body <- renderUI({
+    if (USER$login == TRUE ) {
+      tabItems(
+        
+        # First tab
+        tabItem(tabName ="dashboard", class = "active",
+                fluidRow(
+                  box(width = 12, dataTableOutput('results'))
+                )),
+        
+        # Second tab
+        tabItem(tabName = "second",
+                fluidRow(
+                  box(width = 12, dataTableOutput('results2'))
+                )
+        ))
+      
+    }
+    else {
+      loginpage
+    }
+  })
+  
+  output$results <-  DT::renderDataTable({
+    datatable(iris, options = list(autoWidth = TRUE,
+                                   searching = FALSE))
+  })
+  
+  output$results2 <-  DT::renderDataTable({
+    datatable(mtcars, options = list(autoWidth = TRUE,
+                                     searching = FALSE))
+  })
+  
+  
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+runApp(list(ui = ui, server = server), launch.browser = TRUE)
